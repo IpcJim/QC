cb042be4c13bd701298ba6708873c92dc94f81d5
Jim Nguyen <jimnguyen@Service-iMac1.local>
2014-07-21 12:04:39 -0700

    Initial Commit

---
+++ QC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  QC
+//
+//  Created by Jim Nguyen on 7/21/14.
+//  Copyright (c) 2014 Infinite Peripherals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
